208cd994312f46f8dcf9ca7ecec682b544dec832
Jeff Scott Ward <jsward.17@gmail.com>
2016-04-12 17:43:27 -0400

    upgrade to solc latest

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var solc = require('solc');
 
-module.exports = function(source) {  
-	
-	this.cacheable(); 
+module.exports = function(source) {
+
+	this.cacheable();
 
 	var compiledSOLfile = solc.compile(source);
 
-	return compiledSOLfile; 
+	return compiledSOLfile;
 }

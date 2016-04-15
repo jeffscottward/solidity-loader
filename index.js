e8812780684b117a9a9a23f2dea5675cabccf5ee
Jeff Scott Ward <jsward.17@gmail.com>
2016-04-14 21:16:31 -0400

    working definitely when loaders are in the right order - pudding comes first

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var solc = require('solc');
 
 module.exports = function(source) {
 
+	// console.log(source);
+
 	this.cacheable();
 
 	var compiledSOLfile = solc.compile(source, 1);
+
+	// console.log(compiledSOLfile);
 
 	return compiledSOLfile;
 }
